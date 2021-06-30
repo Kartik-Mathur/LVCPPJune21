18aa0211a5060ce7d813eb2577cd206fd24fba13
kartik mathur <kartik.mathur10@gmail.com>
2021-06-30 23:02:18 +0530

    Basics of Programming

---
+++ Lecture-1/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Headerfile: To add input and output functionality in our program
+using namespace std; // That neeche ka code jo hoga, usmei jo keywords hai unke mtlb
+// jo standard namespace ke andar likhe hai vo hote h
+
+// To build : to convert your code to executable (cmd+b)
+// Windows : Powershell
+// Mac, Linux : Terminal
+// To see the current files in the folder or the location: DIR
+int main() {
+
+	cout << "Hello World!";
+	// console output
+	return 0;
+}
---
+++ Lecture-1/Input.cpp
@@ -0,0 +1,52 @@
+// Input.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// unsigned long long int a; // 8 bytes(64bits), Range: [0, 2^64 - 1]
+	// long long int a; // 8 bytes(64bits), Range: [-2^63, 2^63 - 1]
+	// int a;
+	// int b;
+	// int c;
+	// int a = 10, b = 20, c = 1, d = 2;
+	int a , b, c, d;
+	a = 10;
+	b = 20;
+	c = 1;
+	d = 2;
+
+	// To take input we use cin
+	// cin >> a;
+	// cin >> b;
+	// cin >> c;
+	// cin >> a >> b >> c;
+	// To print on the console we use cout
+	// cout << "User Entered a: ";
+	// cout << a;
+	// cout << '\n';
+	cout << "User Entered a: " << a << '\n';
+	// cout << "User Entered b: ";
+	// cout << b;
+	// cout << '\n';
+	cout << "User Entered b: " << b << '\n';
+	cout << "User Entered c: " << c << '\n';
+	cout << "User Entered d: " << d << '\n';
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

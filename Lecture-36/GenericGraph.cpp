45a68a8f54da15fb38b2279823a3686f66ffd91b
kartik mathur <kartik.mathur10@gmail.com>
2021-09-04 22:14:03 +0530

    Graphs

--- Lecture-36/GenericGraph.cpp
+++ Lecture-36/GenericGraph.cpp
@@ -86,14 +86,34 @@ public:
 
 int main() {
 	Graph<int> g;
+	int board[50] = {0};
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[17] = -13;
+	board[20] = -14;
+	board[24] = -8;
+	board[25] = -10;
+	board[32] = -2;
+	board[34] = -22;
 
-	g.addEdge(1, 0);
-	g.addEdge(1, 2);
-	g.addEdge(4, 2);
-	g.addEdge(4, 5);
-	g.addEdge(3, 0);
-	g.addEdge(3, 2);
-	g.addEdge(3, 4);
+	for (int u = 0 ; u < 36 ; u++) {
+		for (int dice = 1 ; dice <= 6 ; dice++) {
+			int v = u + dice + board[u + dice];
+			g.addEdge(u, v);
+		}
+	}
+
+	cout << g.SSSP(0, 36) << endl;
+
+	// g.addEdge(1, 0);
+	// g.addEdge(1, 2);
+	// g.addEdge(4, 2);
+	// g.addEdge(4, 5);
+	// g.addEdge(3, 0);
+	// g.addEdge(3, 2);
+	// g.addEdge(3, 4);
 
 	// g.addEdge("Modi", "Trump", true);
 	// g.addEdge("Modi", "Yogi", true);
@@ -104,8 +124,8 @@ int main() {
 	// g.addEdge("Putin", "Pope", false);
 
 	// g.print();
-	g.bfs(0);
-	cout << g.SSSP(0, 5) << endl;
+	// g.bfs(0);
+	// cout << g.SSSP(0, 5) << endl;
 	// g.SSSP(0, 5);
 	return 0;
 }
